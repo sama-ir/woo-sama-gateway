ffdaac65ec07374fd5c91b45b092bfbb313e9ff2
Ased Mammad <mammad.ased@gmail.com>
2023-04-19 16:34:27 +0330

    🎨 Reformat code

--- woo-sama-gateway.php
+++ woo-sama-gateway.php
@@ -1,25 +1,31 @@
 <?php
 
-if (!defined('ABSPATH')) exit;
+if (!defined("ABSPATH")) {
+    exit();
+}
 /**
  * Plugin Name: درگاه پرداخت تضمین شده سما
- * Author: علی فروزنده
+ * Author: سامانه معاملات امن ایران (سما)
  * Description: این افزونه درگاه پرداخت تضمین شده سما را به ووکامرس اضافه می کند.
  * Version: 1.0.0
- * Author URI: https://www.ponisha.ir/profile/forouzandea
- * Author Email: forouzandea@gmail.com
+ * Author URI: https://www.sama.ir
+ * Author Email: info@sama.ir
  * Requires at least: 6.0.0
  * Requires PHP: 7.4
  * WC requires at least: 7.3
  * WC tested up to: 7.3
  */
 
-define('WOO_GSAMA_GATEWAY_DIR', trailingslashit(plugin_dir_path(__FILE__)));
+define("WOO_GSAMA_GATEWAY_DIR", trailingslashit(plugin_dir_path(__FILE__)));
 
-require_once WOO_GSAMA_GATEWAY_DIR . 'action.php';
+require_once WOO_GSAMA_GATEWAY_DIR . "action.php";
 
-add_action('woocommerce_before_checkout_form', function () {
-    $sama = new WC_GSama();
-    $message = $sama->before_payment_description;
-    wc_print_notice($message);
-},1);
+add_action(
+    "woocommerce_before_checkout_form",
+    function () {
+        $sama = new WC_GSama();
+        $message = $sama->before_payment_description;
+        wc_print_notice($message);
+    },
+    1
+);
